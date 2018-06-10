6ef18c33d7053956320c8ecd0c244599fb7b70e2
sjeong823 <sjeong823@gmail.com>
2018-06-10 12:54:02 -0400

    v.0.1.0
    v.0.1.0

--- R/plot.predaplm.R
+++
@@ -1,9 +0,0 @@
-plot.predaplm=function(x,kde=FALSE,...){
-	if(kde==TRUE){
-		plot(density(x$posterior.predictive.values),xlab="",ylab="",main="")
-	} else {
-		hist(x$posterior.predictive.values,prob=T,main="",xlab="",ylab=""); box()
-	}
-	mtext("Density",side=2,line=2.3,cex=0.9)
-	mtext("Posterior predictive values",side=1,line=2.3,cex=0.9)
-}
--- R/summary.bmsaplm.R
+++
@@ -1,3 +0,0 @@
-summary.bmsaplm=function(object,...){
-	print(list(inclusion.probability=object$inclusion.probability,latent.variable.estimate=object$latent.variable.estimat))
-}
